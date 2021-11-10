69dca775c274a44b9184233dc44ede6db3553fd4
Xiaohong(Shawn) <aold619@gmail.com>
2021-11-10 20:36:59 +0800

    add 404 & init README

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
-from flask import Flask
-from flask import json
+from flask import Flask, json, request
 import logging
 
 app = Flask(__name__)
 
-@app.route('/status')
-def healthcheck():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Status request successfull')
-    return response
-
-@app.route('/metrics')
-def metrics():
-    response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Metrics request successfull')
-    return response
 
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
-
     return "Hello World!"
 
-if __name__ == "__main__":
-    ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype="application/json"
+    )
+
+    app.logger.info("Status request successfull")
+
+    return response
+
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, \
+            "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype="application/json"
+    )
+
+    app.logger.info("Metrics request successfull")
     
-    app.run(host='0.0.0.0')
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    app.logger.warning("Error Access: {path}".format(path=request.path))
+    return "404 PAGE NOT FOUND"
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
+    app.run(host='0.0.0.0', debug=True)
+
