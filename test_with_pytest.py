68554caa58b502e433543addcb062405320cb806
Xiaohong(Shawn) <aold619@gmail.com>
2021-11-11 19:22:57 +0800

    add test

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -1,3 +1,17 @@
-## Define a test that will always pass successfully
-def test_always_passes():
-    assert True
+# -*- coding: utf-8 -*-
+import app as test_app
+import pytest
+
+
+#@pytest.fixture
+def test_route():
+    with test_app.app.test_client() as c:
+        response = c.get('/')
+        assert b"Hello World!" == response.get_data()
+
+        response = c.get('/duck')
+        assert b"404 PAGE NOT FOUND" == response.get_data()
+
+        response = c.get('/status')
+        assert "OK - healthy" == response.get_json().get('result')
+
